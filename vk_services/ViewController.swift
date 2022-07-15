2e848ab95252e70bc11aee23f847f74a17cb35e6
playatlas <atlasafri@gmail.com>
2022-07-15 08:15:46 +0300

    Initial Commit

---
+++ vk_services/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk_services
+//
+//  Created by Ринат Афиатуллов on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
